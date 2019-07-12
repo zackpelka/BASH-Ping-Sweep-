276f21ea34779250234027633aa9405da3973a71
zackpelka <49172692+zackpelka@users.noreply.github.com>
2019-07-12 16:56:29 -0500

    Updated and removed the grep from executing
    fping -a removed the need for a grep command

--- Bash-Ping-Sweep.sh
+++ Bash-Ping-Sweep.sh
@@ -1,4 +1,5 @@
 CD ~
 ifconfig > Current_IP_Configuration.txt
 fping -a -s -g -t 2000 10.0.2.1 10.0.2.35 > results.txt 
-awk '/alive/ { print $1, $2, $3 }' results.txt > live-ips
+# REM'ed the below line as grep no longer needs to be used with he use of -a in he fping command
+# awk '/alive/ { print $1, $2, $3 }' results.txt > live-ips
