f2c2f0359b666ae8d0021f9b16119a1705f93aad
zackpelka <49172692+zackpelka@users.noreply.github.com>
2019-07-12 16:16:08 -0500

    Changed to detect alive status
    Changed to detect alive status

--- Bash-Ping-Sweep.sh
+++ Bash-Ping-Sweep.sh
@@ -1,4 +1,4 @@
 CD ~
 ifconfig > Current_IP_Configuration.txt
 fping -s -g -t 2000 10.0.2.1 10.0.2.35 > results.txt 
-awk '/unreachable/ { print $1, $2, $3 }' results.txt > results2.txt
+awk '/alive/ { print $1, $2, $3 }' results.txt > results2.txt
