1b58fcf6edd39c65d7fe9bac755d6c004a920714
zackpelka <49172692+zackpelka@users.noreply.github.com>
2019-07-12 16:58:42 -0500

    Simplifying the code

--- Bash-Ping-Sweep.sh
+++ Bash-Ping-Sweep.sh
@@ -1,5 +1,4 @@
-CD ~
 ifconfig > Current_IP_Configuration.txt
-fping -a -s -g -t 2000 10.0.2.1 10.0.2.35 > results.txt 
+fping -a -s -g -t 2000 10.0.2.1 10.0.2.35 > live-ips
 # REM'ed the below line as grep no longer needs to be used with he use of -a in he fping command
 # awk '/alive/ { print $1, $2, $3 }' results.txt > live-ips
