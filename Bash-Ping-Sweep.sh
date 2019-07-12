d82f629148176e1ea2af8b1fe5250f74354cc928
zackpelka <49172692+zackpelka@users.noreply.github.com>
2019-07-12 15:55:15 -0500

    Modified script for valid local network
    Modified script for valid local network

--- Bash-Ping-Sweep.sh
+++ Bash-Ping-Sweep.sh
@@ -1,4 +1,4 @@
 CD ~
 ifconfig > Current_IP_Configuration.txt
-fping -s -g -t 2000 10.179.0.1 10.179.0.35 > results.txt 
+fping -s -g -t 2000 10.0.2.1 10.0.2.35 > results.txt 
 awk '/unreachable/ { print $1, $2, $3 }' results.txt > results2.txt
