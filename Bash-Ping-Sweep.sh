6f169e91d9b41c3d43c6e6028d239c9352c74e8c
zackpelka <49172692+zackpelka@users.noreply.github.com>
2019-07-12 15:31:26 -0500

    Update Bash-Ping-Sweep.sh

--- Bash-Ping-Sweep.sh
+++ Bash-Ping-Sweep.sh
@@ -1,3 +1,4 @@
 CD ~
 ifconfig > Current_IP_Configuration.txt
-fping -s -g -t 2000 10.179.0.1 10.179.0.35 > results.txt | awk '/unreachable/ { print $1, $2, $3 }' results.txt > results2.txt
+fping -s -g -t 2000 10.179.0.1 10.179.0.35 > results.txt 
+awk '/unreachable/ { print $1, $2, $3 }' results.txt > results2.txt
